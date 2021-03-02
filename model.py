fe870b557db84458a3876f677c43fad2fb016628
Tai Le <ltquoctaidn98@gmail.com>
2021-03-02 13:15:20 +0700

    Add model

--- model.py
+++ model.py
@@ -1,31 +1,33 @@
 import torch
 import torch.nn as nn
 
-# Layer type: conv, out_channels, kernel_size, stride
+# Layer type: conv, out_channels, kernel_size
 # Layer type: max pooling, stride
+# Layer type: residual, out_channels, kernel_size
+# Layer type: detection, kernel_size
 ARCHITECTURE = [
     ["conv", 16, 3],
     ["conv", 16, 3],
-    ["max_pooling", 2],
+    ["max_pooling", 2, 2],
 
     ["conv", 32, 3],
     ["res", 32, 3],
-    ["max_pooling", 2],
+    ["max_pooling", 2, 2],
 
     ["conv", 64, 3],
     ["res", 64, 3],
     ["res", 64, 3],
-    ["max_pooling", 2],
+    ["max_pooling", 2, 2],
 
     ["res", 64, 3],
     ["res", 64, 3],
-    ["max_pooling", 2],
+    ["max_pooling", 2, 2],
 
     ["conv", 128, 3],
     ["res", 128, 3],
     ["res", 128, 3],
 
-    ["detection"]
+    ["detection", 3]
 ]
 
 
@@ -64,6 +66,7 @@ class DetectionBlock(nn.Module):
         super().__init__()
         self.conv1 = CNNBlock(in_channels, 2, kernel_size, use_activation_fn=False)
         self.conv2 = CNNBlock(in_channels, 6, kernel_size, use_activation_fn=False)
+        # self.softmax = nn.Softmax(dim=1)      dim=1??
         self.softmax = nn.Softmax()
 
     def forward(self, X):
@@ -76,8 +79,37 @@ class DetectionBlock(nn.Module):
 class WPODNet(nn.Module):
     def __init__(self, in_channels: int = 3):
         super().__init__()
-        self.in_channels = in_channels
+        self.layers = self._create_conv_layers(in_channels)
 
-    def _create_conv_layers(self):
-        pass
+    def forward(self, X):
+        return self.layers(X)
 
+    def _create_conv_layers(self, in_channels):
+        layers = []
+
+        for layer in ARCHITECTURE:
+            layer_name = layer[0]
+
+            if layer_name == "conv":
+                _, out_channels, kernel_size = layer
+                layers.append(CNNBlock(in_channels, out_channels, kernel_size))
+
+                in_channels = out_channels
+            elif layer_name == "max_pooling":
+                _, kernel_size, stride = layer
+                layers.append(nn.MaxPool2d(kernel_size=kernel_size, stride=stride))
+            elif layer_name == "res":
+                _, out_channels, kernel_size = layer
+                layers.append(ResidualBlock(in_channels, out_channels, kernel_size))
+
+                in_channels = out_channels
+            elif layer_name == "detection":
+                layers.append(DetectionBlock(in_channels, 3))
+
+        return nn.Sequential(*layers)
+
+
+if __name__ == "__main__":
+    X = torch.rand((1, 3, 120, 80))
+    wpod_net = WPODNet()
+    print(wpod_net(X).shape)
